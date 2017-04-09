6ad65d8ed9a1670c8ef9ae3ec8d14208c6f8d214
deepakkasera <deepak.kasera143.dk@gmail.com>
2017-04-09 18:11:01 +0530

    ViewPager2

---
+++ app/src/main/java/com/example/deepak/viewpager2/Tab3.java
@@ -0,0 +1,89 @@
+package com.example.deepak.viewpager2;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.deepak.viewpager2.model.PlacementRecords;
+import com.example.deepak.viewpager2.model.PlacementRecordsYear;
+
+import java.util.ArrayList;
+
+/**
+ * Created by deepak on 8/4/17.
+ */
+
+public class Tab3 extends Fragment {
+
+    RecyclerView rvPlacementsRecords  ;
+    ArrayList<PlacementRecordsYear> placementRecordsYearArrayList ;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.tab3, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        placementRecordsYearArrayList = new ArrayList<PlacementRecordsYear>();
+        placementRecordsYearArrayList.add(new PlacementRecordsYear(2010));
+        placementRecordsYearArrayList.add(new PlacementRecordsYear(2011));
+        placementRecordsYearArrayList.add(new PlacementRecordsYear(2012));
+        placementRecordsYearArrayList.add(new PlacementRecordsYear(2013));
+
+        rvPlacementsRecords = (RecyclerView) getActivity().findViewById(R.id.rvPlacmentsRecords);
+        PlacementRecordsYearAdapter placementRecordsYearAdapter = new PlacementRecordsYearAdapter();
+        rvPlacementsRecords.setLayoutManager(new LinearLayoutManager(getActivity()));
+        rvPlacementsRecords.setAdapter(placementRecordsYearAdapter);
+    }
+
+    class PlacmentRecordsyearHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+
+        TextView tvRecordYear;
+        public PlacmentRecordsyearHolder(View itemView) {
+            super(itemView);
+            itemView.setOnClickListener(this);
+            this.tvRecordYear = (TextView) itemView.findViewById(R.id.tvRecordYear);
+        }
+
+        @Override
+        public void onClick(View v) {
+            //Start new Activity
+        }
+    }
+
+
+    class PlacementRecordsYearAdapter extends RecyclerView.Adapter<PlacmentRecordsyearHolder>{
+
+        @Override
+        public PlacmentRecordsyearHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            LayoutInflater layoutInflater = getLayoutInflater(null);
+            
+            View itemView;
+            itemView = layoutInflater.inflate(R.layout.rv_placement_records_year,parent,false);
+            return new PlacmentRecordsyearHolder(itemView);
+        }
+
+        @Override
+        public void onBindViewHolder(PlacmentRecordsyearHolder holder, int position) {
+            PlacementRecordsYear placementRecordsYear = placementRecordsYearArrayList.get(position);
+            holder.tvRecordYear.setText(placementRecordsYear.getYear());
+        }
+
+        @Override
+        public int getItemCount() {
+            return 0;
+        }
+    }
+}
+
+
---
+++ app/src/main/java/com/example/deepak/viewpager2/model/PlacementRecordsYear.java
@@ -0,0 +1,21 @@
+package com.example.deepak.viewpager2.model;
+
+/**
+ * Created by deepak on 9/4/17.
+ */
+
+public class PlacementRecordsYear {
+    int year;
+
+    public PlacementRecordsYear(int year) {
+        this.year = year;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+}
